2609be0acb19004b8a471324207c04efb51025f6
arjun vr <arjarjun2004@gmail.com>
2024-07-17 21:33:45 +0530

    add/production path@app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,15 @@ import  userRoutes  from "./routes/userRoutes.js";
 import { connectDB } from "./database/dbConfig.js";
 import cors from 'cors'
 
+import path from "path"
+const currentWorkingDir = path.resolve();
+const parentDir = path.dirname(currentWorkingDir)
+const productionParendDir = path.dirname(parentDir)
+console.log('currentworkingdir:',currentWorkingDir)
+console.log('parendDir:',parentDir)
+console.log('productiondir:',productionParendDir)
+
+
 const app: Application = express();
 
 connectDB();
@@ -13,6 +22,23 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.use('/api',userRoutes)
+
+const environments = 'Production'
+
+if ( environments === 'Production') { 
+  app.use(express.static(path.join( productionParendDir , '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve( productionParendDir , 'frontend', 'dist', 'index.html'))
+  );
+
+} else {
+  app.use(express.static(path.join( parentDir , '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve( parentDir , 'frontend', 'dist', 'index.html'))
+  );
+}
 
 const port: number = 3500;
 
